eedd49a154e413d50bfd7e7edb19ae392b4d0dd7
madisonk341-tech <madisonk341@gmail.com>
2026-05-31 19:30:36 -0500

    Add files via upload

---
+++ user_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout, std::endl, std::cin;
+
+int main(){
+    int number = 0;
+    cout<<"Enter a number: ";
+    cin>>number;
+    cout<<"You entered: "<<number <<"." <<endl;
+    return 0;
+}
